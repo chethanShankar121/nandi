fa82848d3853a0ce20fe5c7f2df8ed0834bffcae
ravitejav-Accolite <raviteja.v@accoliteoindia.com>
2020-02-16 20:08:56 +0530

    login api modified

--- backendApi/src/main/java/com/nldc/build/model/AuthModel.java
+++ backendApi/src/main/java/com/nldc/build/model/AuthModel.java
@@ -6,20 +6,30 @@ public class AuthModel {
 	private String authMessage;
 	private String sessionID;
 	private String name;
+	private int accessRole;
 	
 	public String getName() {
 		return name;
+	}
+
+	public int getAccessRole() {
+		return accessRole;
+	}
+
+	public void setAccessRole(int accessRole) {
+		this.accessRole = accessRole;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public AuthModel(int authStatus, String authMessage, String sessionID, String name) {
+	public AuthModel(int authStatus, String authMessage, String sessionID, String name,int role) {
 		this.setAuthStatus(authStatus);
 		this.setAuthMessage(authMessage);
 		this.setSessionID(sessionID);
 		this.setName(name);
+		this.setAccessRole(role);
 	}
 	
 	public int getAuthStatus() {
---
+++ backendApi/src/main/java/com/nldc/build/query/UserQuery.java
@@ -0,0 +1,7 @@
+package com.nldc.build.query;
+
+public class UserQuery {
+	
+	public static String LOGIN_QUERY = "SELECT * FROM user where userName=? and password=?";
+
+}
