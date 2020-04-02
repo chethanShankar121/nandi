05ad2680f535f4e51daffadd639dcfd221a8e7f1
ravitejav-Accolite <raviteja.v@accoliteindia.com>
2020-04-02 19:13:30 +0530

    Added login after successful verification only

--- backendApi/src/main/java/com/nldc/build/jparepository/UserJpaRepository.java
+++ backendApi/src/main/java/com/nldc/build/jparepository/UserJpaRepository.java
@@ -9,10 +9,10 @@ import com.nldc.build.model.User;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
 	
-	@Query("Select u from User u where u.email = ?1 and u.password = ?2")
+	@Query("Select u from User u where u.email = ?1 and u.password = ?2 and email_verifed=true and phone_number_verifed=true")
 	List<User> loginUser(String email, String password);
 	
-	@Query("Select u from User u where u.phone = ?1 and u.password = ?2")
+	@Query("Select u from User u where u.phone = ?1 and u.password = ?2 and email_verifed=true and phone_number_verifed=true")
 	List<User> loginUserPhoneNumber(String phoneNumber, String password);
 	
 	
